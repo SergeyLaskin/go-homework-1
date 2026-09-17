5acb86f2015529142f8ea77bab535ef2eedd0256
SergeyLaskin <laskin2022@mail.ru>
2026-09-17 21:37:25 +0300

    Добавил дз по го

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	seen := make(map[int]int)
+
+	for i, num := range nums {
+		need := target - num
+
+		if j, ok := seen[need]; ok {
+			return []int{j, i}
+		}
+
+		seen[num] = i
+	}
+
+	return []int{}
+}
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+
+	result := twoSum(nums, target)
+
+	fmt.Println(result)
+}
